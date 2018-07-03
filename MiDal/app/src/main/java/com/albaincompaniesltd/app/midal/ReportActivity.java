d511194f81964b1e1aabd159405fec1e3efd5ff4
Ted <ted@redpulse.co.ke>
2018-07-03 09:10:44 +0300

    added new fields to capture user data

--- MiDal/app/src/main/java/com/albaincompaniesltd/app/midal/ReportActivity.java
+++ MiDal/app/src/main/java/com/albaincompaniesltd/app/midal/ReportActivity.java
@@ -3,9 +3,18 @@ package com.albaincompaniesltd.app.midal;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class ReportActivity extends AppCompatActivity {
+
+    EditText idnumber,fullnames,paxCar,pNumber,officevisiting;
+    Button sendData;
+    ProgressBar progressBar;
+
 
 
 
@@ -21,6 +30,35 @@ public class ReportActivity extends AppCompatActivity {
         String display = intent.getString("PlateNumber");
         displayPlateNumber = findViewById(R.id.plateDisplay);
        displayPlateNumber.setText(display);
+       sendData = findViewById(R.id.btnSubmit);
+       idnumber = findViewById(R.id.edtIDnumber);
+       fullnames = findViewById(R.id.edtfullnames);
+       paxCar = findViewById(R.id.edtPaxNumber);
+       pNumber = findViewById(R.id.edtPhone);
+       officevisiting = findViewById(R.id.edtOffice);
+
+
+
+
+
+       sendData.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               //after submitiing data send it to the dashboard
+
+
+
+               String ID = idnumber.getText().toString().trim();
+               String fNames = fullnames.getText().toString().trim();
+               String passengers = paxCar.getText().toString().trim();
+               String phoneNumber = pNumber.getText().toString().trim();
+               String visiting = officevisiting.getText().toString().trim();
+
+               //Start async task operations
+
+
+           }
+       });
 
 
 
